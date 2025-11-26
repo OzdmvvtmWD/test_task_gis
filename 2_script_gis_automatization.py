0a19a28be1c61a13af3278197c4b632c5f9a4bd2
Ghoul <ia.02guzbo@gmail.com>
2025-11-27 01:38:27 +0200

    'fix_2'

--- 2_script_gis_automatization.py
+++ 2_script_gis_automatization.py
@@ -15,18 +15,29 @@ gis_custom = GISDriver(
 )
 
 test_item = gis_custom.get_item(TEST_ITEM_ID)
+test_layer = test_item.layers[0]
 
-gis_custom.ensure_folder(WORK_FOLDER_NAME)
-test_item_copy = gis_custom.clone_item_to_folder(TEST_ITEM_ID, WORK_FOLDER_NAME)
-test_layer_copy  = test_item_copy.layers[0]
+print(test_layer.properties['capabilities'])
 
-print(test_item_copy.id)
-gis_custom.get_layer_fields(test_layer_copy)
+# gis_custom.ensure_folder(WORK_FOLDER_NAME)
+# test_item_copy = gis_custom.clone_item_to_folder(TEST_ITEM_ID, WORK_FOLDER_NAME)
+# test_layer_copy  = test_item_copy.layers[0]
+# print(test_layer_copy.properties['capabilities'])
 
-gis_custom.delete_all_features(test_layer_copy)
-gis_custom.add_csv(test_layer_copy, CSV_PATH)
+print(test_item.id)
+gis_custom.get_layer_fields(test_layer)
 
-gis_custom.delete_garbage_field(test_layer_copy,'Область')
-gis_custom.delete_garbage_field(test_layer_copy,'date_1')
-gis_custom.delete_garbage_field(test_layer_copy,'date__')
-gis_custom.delete_garbage_field(test_layer_copy,'GlobalID')
+gis_custom.delete_all_features(test_layer)
+gis_custom.add_csv(test_layer, CSV_PATH)
+
+
+"""
+for deleting garbage_field, if i try it calls 
+Exception: User does not have permissions to access this service
+User does not have permissions to access this service
+(Error Code: 403)
+"""
+# gis_custom.delete_garbage_field(test_layer,'Область')
+# gis_custom.delete_garbage_field(test_layer,'date_1')
+# gis_custom.delete_garbage_field(test_layer,'date__')
+# gis_custom.delete_garbage_field(test_layer,'GlobalID')
